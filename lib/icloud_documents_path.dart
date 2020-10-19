3fcefd4ba6c1897da1d3a67608dd30eeaa71a9b8
Vishesh Handa <me@vhanda.in>
2020-10-19 19:44:50 +0200

    Rename function

--- lib/icloud_documents_path.dart
+++ lib/icloud_documents_path.dart
@@ -6,7 +6,7 @@ class ICloudDocumentsPath {
   static const MethodChannel _channel =
       const MethodChannel('icloud_documents_path');
 
-  static Future<String> get platformVersion async {
+  static Future<String> get documentsPath async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
