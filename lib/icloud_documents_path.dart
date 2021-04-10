d7bfaa69eb8758dcde273e8ea69e0197d2badbb4
Vishesh Handa <me@vhanda.in>
2021-04-10 21:38:24 +0200

    null safety migration

--- lib/icloud_documents_path.dart
+++ lib/icloud_documents_path.dart
@@ -6,8 +6,8 @@ class ICloudDocumentsPath {
   static const MethodChannel _channel =
       const MethodChannel('icloud_documents_path');
 
-  static Future<String> get documentsPath async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get documentsPath async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 }
